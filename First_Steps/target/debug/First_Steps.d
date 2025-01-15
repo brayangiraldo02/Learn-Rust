d822c018adcb62bdb368ee7cce846eb49a9a466f
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2025-01-14 22:47:55 -0500

    Hello World Rust!

---
+++ First_Steps/target/debug/First_Steps.d
@@ -0,0 +1,1 @@
+/home/giraldo/Personal/Repositorios\ Github/Learn\ Rust!/First_Steps/target/debug/First_Steps: /home/giraldo/Personal/Repositorios\ Github/Learn\ Rust!/First_Steps/src/main.rs
---
+++ First_Steps/target/debug/deps/First_Steps-d6da2ef2d08d8f8e.d
@@ -0,0 +1,5 @@
+/home/giraldo/Personal/Repositorios Github/Learn Rust!/First_Steps/target/debug/deps/First_Steps-d6da2ef2d08d8f8e: src/main.rs
+
+/home/giraldo/Personal/Repositorios Github/Learn Rust!/First_Steps/target/debug/deps/First_Steps-d6da2ef2d08d8f8e.d: src/main.rs
+
+src/main.rs:
